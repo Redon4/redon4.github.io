32404974ed504b128a39aa8d6853e22dfbc1c61b
Redon4 <211563626+redon4@users.noreply.github.com>
2026-04-17 22:24:32 +0200

    v1.1.11: begun with main page, stole load func from other project

--- version_load.js
+++ version_load.js
@@ -1,3 +1,18 @@
 const version = "v1.1.10";
 
 document.getElementById("ver").innerText = version;
+
+async function load_stuff() {
+    const jobs = [...document.querySelectorAll("[data-include]")].map( //array of everything that has [data-include], loops through the array
+        el =>
+            fetch(el.dataset.include)
+            .then(r => r.text())
+            .then(html => el.innerHTML = html)
+    );
+
+
+    return Promise.all(jobs);
+}
+
+
+document.addEventListener("DOMContentLoaded", load_stuff);
